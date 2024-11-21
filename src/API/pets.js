aad6690a071138b3057cd0e492615ea177ceb0c6
MrHasak99 <hamad_alkhalaf_99@hotmail.com>
2024-11-21 17:20:32 +0300

    Task Finished

---
+++ src/API/pets.js
@@ -0,0 +1,18 @@
+import instance from "./index";
+
+async function getAllPets() {
+  const response = await instance.get("/pets");
+  return response;
+}
+
+async function getPetById(id) {
+  const response = await instance.get(`/pets/${id}`);
+  return response;
+}
+
+async function addPet(data) {
+  const response = await instance.post("/pets", data);
+  return response;
+}
+
+export { getAllPets, getPetById, addPet };
